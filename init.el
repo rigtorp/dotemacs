de6bd4bede4c446d05758ff892d8369fc7172699
Erik Rigtorp <erik@rigtorp.se>
2015-11-26 16:46:02 -0500

    Upgrade to emacs24

--- init.el
+++ init.el
@@ -1,108 +1,51 @@
-;; basic settings
-(set-default-font "Monospace-10")
-(menu-bar-mode nil)
-(scroll-bar-mode nil)
-(tool-bar-mode nil)
-(setq inhibit-startup-screen t)
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
 
-(add-to-list 'load-path "~/.emacs.d/")
+(add-hook 'after-init-hook #'global-flycheck-mode)
+(add-hook 'go-mode-hook 'auto-complete-mode)
 
-;; iflipb
-(require 'iflipb)
-(global-set-key (kbd "<C-right>") 'iflipb-next-buffer)
-(global-set-key (kbd "<C-left>") 'iflipb-previous-buffer)
+(add-hook 'after-init-hook 'global-company-mode)
+(add-hook 'after-init-hook 'yas-global-mode)
 
-;; flyspell
-(dolist (hook '(text-mode-hook))
-  (add-hook hook (lambda () (flyspell-mode 1))))
+(eval-after-load 'company
+  '(add-to-list 'company-backends 'company-irony))
 
-;; yasnippet
-(add-to-list 'load-path "~/.emacs.d/yasnippet")
-(require 'yasnippet)
-(yas/initialize)
-(yas/load-directory "~/.emacs.d/yasnippet/snippets")
+(eval-after-load 'company
+  '(add-to-list 'company-backends '(company-irony-c-headers company-irony)))
 
-;; auto complete
-(add-to-list 'load-path "~/.emacs.d/auto-complete")
-(require 'auto-complete-config)
-(add-to-list 'ac-dictionary-directories 
-             "~/.emacs.d/auto-complete/ac-dict")
-(ac-config-default)
+(add-hook 'c++-mode-hook 'irony-mode)
+(add-hook 'c-mode-hook 'irony-mode)
+(add-hook 'irony-mode-hook 'irony-cdb-autosetup-compile-options)
+(setq irony-additional-clang-options '("-std=c++14"))
 
-;; gccsense
-(require 'gccsense)
-(add-hook 'c-mode-common-hook
-          (lambda ()
-            (local-set-key (kbd "C-c .") 'ac-complete-gccsense)))
+(eval-after-load 'flycheck
+  '(add-hook 'flycheck-mode-hook #'flycheck-irony-setup))
 
-;; gtags
-;(add-hook 'c-mode-common-hook
-;          (lambda ()
-;            (gtags-mode)))
+(add-to-list 'auto-mode-alist '("\\.h\\'" . c++-mode))
 
-;; anything
-(add-to-list 'load-path "~/.emacs.d/anything")
-(require 'anything)
-(require 'anything-config)
-(require 'anything-git)
+(add-hook 'after-init-hook 'projectile-global-mode)
 
-(defun anything-default ()
-  (interactive)
-  (setq anything-default-directory default-directory)
-  (anything-other-buffer
-   '(anything-c-source-buffers
-     anything-c-source-recentf
-     anything-c-source-files-in-current-dir
-     anything-c-source-git-files
-     anything-c-source-man-pages
-     anything-c-source-locate
-     anything-c-source-emacs-commands
-     anything-c-source-calculation-result)
-   nil))
+(global-set-key
+ (kbd "<f5>") (lambda ()
+		(interactive)
+		(setq-local compilation-read-command nil)
+		(call-interactively 'compile)))
 
-(defun anything-for-current-buffer ()
-  (interactive)
-  (setq anything-default-directory default-directory)
-  (anything-other-buffer
-   '(anything-c-source-occur
-     anything-c-source-fixme
-     anything-c-source-imenu
-     anything-c-source-ctags)
-   "*anything current buffer*"))
-
-(defun anything-for-current-project ()
-  (interactive)
-  (setq anything-default-directory default-directory)
-  (anything-other-buffer
-   '(anything-c-source-git-grep
-     anything-c-source-ctags)
-   "*anything current project*"))
-
-
-(global-set-key (kbd "C-x C-a") 'anything-default)
-(global-set-key (kbd "C-x C-b") 'anything-for-current-buffer)
-(global-set-key (kbd "C-x p") 'anything-for-current-project)
-
-;; c-mode
-(c-set-offset 'innamespace 0)
-
-;; custom
 (custom-set-variables
-  ;; custom-set-variables was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
- '(column-number-mode t)
- '(ido-mode t nil (ido))
- '(indent-tabs-mode nil)
- '(recentf-mode t)
- '(savehist-mode t nil (savehist))
- '(show-paren-mode t)
- '(standard-indent 2)
- '(use-file-dialog nil))
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(clang-format-executable "clang-format-3.7")
+ '(safe-local-variable-values
+   (quote
+    ((eval add-hook
+	   (quote before-save-hook)
+	   (function clang-format-buffer)
+	   nil t)))))
 (custom-set-faces
-  ;; custom-set-faces was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
  )
